57c17dc83e177533ee2a94a5fcb6a6b20f2ccd01
Wyatt Barnes <wyattb413@gmail.com>
2016-09-29 09:09:38 -0500

    Basic user prompt, prompting if user would like to deposit or withdrawl

--- checkBook.php
+++ checkBook.php
@@ -3,3 +3,8 @@
 echo"===================================================================================" . PHP_EOL;
 echo"------------------Hello! Welcome to Wyatt's CheckBook Application------------------" . PHP_EOL;
 echo"===================================================================================" . PHP_EOL;
+
+//                     USER INPUT PROMPTS
+  fwrite(STDOUT, "Would you like to Deposit (d) or Withdrawl (w)?" . PHP_EOL);
+  $userInput = trim(fgets(STDIN));
+//
