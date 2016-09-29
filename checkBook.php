32f377a8f1e6c6b015159c77ff1456a125982fe4
Wyatt Barnes <wyattb413@gmail.com>
2016-09-29 10:53:59 -0500

    Added Deposit, Withdrawl and UserInput Check functions to deal with user input

--- checkBook.php
+++ checkBook.php
@@ -8,3 +8,25 @@ echo"===========================================================================
   fwrite(STDOUT, "Would you like to Deposit (d) or Withdrawl (w)?" . PHP_EOL);
   $userInput = trim(fgets(STDIN));
 //
+
+//                     FUNCTION DEFINITIONS
+  function deposit() {
+    fwrite(STDOUT, "How much would you like to deposit?" . PHP_EOL);
+  }
+
+  function withdrawl() {
+    fwrite(STDOUT, "How much would you like to withdrawl?" . PHP_EOL);
+  }
+
+  function userInputCheck() {
+    do {
+      fwrite(STDOUT, "Please enter either 'd' or 'w', thank you!" . PHP_EOL);
+      $userInput = trim(fgets(STDIN));
+    } while ($userInput != "d" && $userInput != "w");
+    if ($userInput == "d") {
+      deposit();
+    } else if ($userInput == "w") {
+      withdrawl();
+    }
+  }
+//
