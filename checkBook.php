2f8a7b782ce9253b7f5ff8245cfe3c0a7b2e288c
Wyatt Barnes <wyattb413@gmail.com>
2016-09-30 21:34:12 -0500

    Have basic deposit functinality: You can select deposit from "transactionPrompt", specify how much you would like to deposit, confirm the amount, and recieve a message with how much you've deposited

--- checkBook.php
+++ checkBook.php
@@ -4,46 +4,59 @@ echo"===========================================================================
 echo"------------------Hello! Welcome to Wyatt's CheckBook Application------------------" . PHP_EOL;
 echo"===================================================================================" . PHP_EOL;
 
-//                     USER INPUT PROMPTS
-  fwrite(STDOUT, "Would you like to Deposit (d) or Withdrawl (w)?" . PHP_EOL);
-  $userInput = trim(fgets(STDIN));
-//
+//                     VARIBLE DEFINITION
+  $depositObject = [];
+  $userInput = "";
 
 //                     FUNCTION DEFINITIONS
-  function deposit() {
-    fwrite(STDOUT, "How much would you like to deposit?" . PHP_EOL);
-  }
-
-  function withdrawl() {
-    fwrite(STDOUT, "How much would you like to withdrawl?" . PHP_EOL);
-  }
-
-  function userInputCheck() {
-    do {
-      fwrite(STDOUT, "Please enter either 'd' or 'w', thank you!" . PHP_EOL);
+  //TRANSACTION PROMPT
+    function transactionPrompt()
+    {
+      fwrite(STDOUT, "Would you like to Deposit (d) or Withdrawl (w)?" . PHP_EOL);
       $userInput = trim(fgets(STDIN));
-    } while ($userInput != "d" && $userInput != "w");
-    if ($userInput == "d") {
-      deposit();
-    } else if ($userInput == "w") {
-      withdrawl();
+  //SWITCH CASE TO DEAL WITH TRANSACTION PROMPT INPUT
+      switch ($userInput) {
+        case "d": {
+          depositPrompt();
+          break;
+        }
+        case "w": {
+          withdrawl();
+          break;
+        }
+        default: {
+          userInputCheck();
+          break;
+        }
+      }
     }
-  }
-//
 
-//                    SWITCH CASE TO DEAL WITH USERINPUT
-switch ($userInput) {
-  case "d": {
-    deposit();
-    break;
-  }
-  case "w": {
-    withdrawl();
-    break;
-  }
-  default: {
-    userInputCheck();
-    break;
-  }
-}
-//
+  //DEPOSIT FUNCTION DEFINITIONS
+    function depositPrompt() {
+      fwrite(STDOUT, "How much would you like to deposit?" . PHP_EOL);
+      $depositAmount = intval(trim(fgets(STDIN)));
+      fwrite(STDOUT, "Are you sure you would like to deposit $" . $depositAmount . "?" . " (Y/N)". PHP_EOL);
+      $depositConformation = trim(strtolower((fgets(STDIN))));
+      if ($depositConformation == "y") {
+        $depositObject ['depositAmount'] = $depositAmount;
+        $depositObject ['depositConformation'] = $depositConformation;
+        deposit($depositObject);
+      } else if ($depositConformation == "n") {
+        transactionPrompt();
+      }
+    }
+
+    function deposit($depositObject) {
+      fwrite(STDOUT, "You have deposited $" . $depositObject['depositAmount'] . PHP_EOL);
+    }
+
+    function withdrawl() {
+      fwrite(STDOUT, "How much would you like to withdrawl?" . PHP_EOL);
+    }
+
+    function userInputCheck() {
+      do {
+        fwrite(STDOUT, "Please enter either 'd' or 'w', thank you!" . PHP_EOL);
+        $userInput = trim(fgets(STDIN));
+      } while ($userInput != "d" && $userInput != "w");
+    }
